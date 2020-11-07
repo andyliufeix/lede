5415ebbaf0642cf24e863e29978b880b8cc59dde
andyliufeix <42169194+andyliufeix@users.noreply.github.com>
2020-11-07 19:18:42 +0800

    Update target.mk

--- include/target.mk
+++ include/target.mk
@@ -16,7 +16,7 @@ DEVICE_TYPE?=router
 DEFAULT_PACKAGES:=base-files libc libgcc busybox dropbear mtd uci opkg netifd fstools uclient-fetch \
 logd urandom-seed urngd block-mount coremark kmod-nf-nathelper kmod-nf-nathelper-extra \
 kmod-ipt-raw wget curl htop ipv6helper libustream-openssl ca-certificates default-settings \
-luci luci-app-adbyby luci-app-sfe luci-app-adbyby-plus luci-app-ddns luci-app-upnp luci-app-autoreboot \
+luci-app-adbyby luci-app-sfe luci-app-adbyby-plus luci-app-ddns luci-app-upnp luci-app-autoreboot \
 luci-app-webadmin luci-app-ssr-plus luci-app-unblockmusic luci-app-arpbind luci-app-wol \
 luci-app-ramfree luci-app-ssr-plus_INCLUDE_Shadowsocks luci-app-ssr-plus_INCLUDE_Simple_obfs \
 luci-app-ssr-plus_INCLUDE_V2ray_plugin luci-app-ssr-plus_INCLUDE_V2ray luci-app-ssr-plus_INCLUDE_Trojan \
